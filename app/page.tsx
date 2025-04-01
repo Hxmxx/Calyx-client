73d9a8c5b3dca1847ff90f3bfaa3e2415ceb8e96
조하민 <chohamin0611@gmail.com>
2025-04-01 16:49:06 +0900

    ADD :: 테마 프로바이더 전체 적용

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home({Component, pageProps}: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <Component {...pageProps} /> {/* 페이지 컴포넌트 */}
     </ThemeProvider>
   );
 }

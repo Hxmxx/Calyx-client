1debff16ed1985fffb3004cf9606c9970d5c6704
조하민 <chohamin0611@gmail.com>
2025-04-01 16:46:19 +0900

    ADD :: 테마 프로바이더 전체 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import {AppProps} from "next/app";
+import {ThemeProvider} from "styled-components";
+import {GlobalStyles} from "../styles/GlobalStyles";
+import {theme} from "../styles/Theme";
 
-
-export default function Home() {
+export default function Home({Component, pageProps}: AppProps) {
   return (
-    <>
-
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
